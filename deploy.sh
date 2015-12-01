807c894e46924573a35377eb3aab71c5a574307e
Sakhile Masoka <sakhile.harvey@gmail.com>
2015-12-01 22:23:23 +0200

    Created deploy

---
+++ deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -e
+# this should be run after check-build finishes.
+. /etc/profile.d/modules.sh
+echo ${SOFT_DIR}
+module add deploy
+module load torque/2.5.13
+module load gcc/$GCC_VERSION
+
+echo ${SOFT_DIR}
+cd ${WORKSPACE}/${NAME}-${VERSION}
+echo "All tests have passed, will now build into ${SOFT_DIR}"
+FC=`which gfortran` CC=`which gcc` CXX=`which g++` ./configure --prefix=${SOFT_DIR}/gcc-${GCC_VERSION} --enable-heterogeneous --enable-mpi-thread-multiple --with-tm
+make install
+echo "Creating the modules file directory ${LIBRARIES_MODULES}"
+mkdir -p ${LIBRARIES_MODULES}/${NAME}
+(
+cat <<MODULE_FILE
+#%Module1.0
+## $NAME modulefile
+##
+proc ModulesHelp { } {
+    puts stderr "       This module does nothing but alert the user"
+    puts stderr "       that the [module-info name] module is not available"
+}
+
+module load torque/2.5.13
+module load gcc/$GCC_VERSION
+
+module-whatis   "$NAME $VERSION : See https://github.com/SouthAfricaDigitalScience/OpenMPI-deploy"
+setenv       OPENMPI_VERSION       $VERSION
+setenv       OPENMPI_DIR           $::env(CVMFS_DIR)/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
+prepend-path 	 PATH            $::env(OPENMPI_DIR)/bin
+prepend-path    PATH            $::env(OPENMPI_DIR)/include
+prepend-path    PATH            $::env(OPENMPI_DIR)/bin
+prepend-path    MANPATH         $::env(OPENMPI_DIR)/man
+prepend-path    LD_LIBRARY_PATH $::env(OPENMPI_DIR)/lib
+MODULE_FILE
+) > ${LIBRARIES_MODULES}/${NAME}/${VERSION}-gcc-$GCC_VERSION
